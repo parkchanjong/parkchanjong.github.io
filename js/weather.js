7345798fde7244f50f9f01818e1c4e6684c815cb
parkchanjong <7000cj@gmail.com>
2020-12-30 22:11:30 +0900

    blog setting

---
+++ js/weather.js
@@ -0,0 +1,69 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "3c74c81731ffabbe5f3633deb435b017";
+const COORDS = "coords";
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      const icon = json.weather[0].main;
+      const weathercase = {
+        Rain: "🌧️",
+        Clear: "☀️",
+        Thunderstorm: "🌩️",
+        Clouds: "☁️",
+        Snow: "⛄",
+        Drizzle: "",
+        Haze: "🌫️",
+        Mist: "🌁",
+      };
+      weather.innerText = `${temperature} °C  
+       ${place} ${weathercase[icon]}`;
+    });
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+  console.log("Cant access geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
