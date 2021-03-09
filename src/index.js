be23f7ba8c4398a76bb7d644ee0638438af29d1f
parkchanjong <7000cj@gmail.com>
2021-03-09 22:34:20 +0900

    script defet 추가

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./index.css";
+const main = require("../js/main");
+const clock = require("../js/clock");
+const weather = require("../js/weather");
+
+main();
+clock();
+weather();
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "none",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    port: 4000,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
